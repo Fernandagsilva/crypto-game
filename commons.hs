687f472ab956307c786f5bbb53b0bc063bbae4ab
Fgsilva <fgsilva@ice.ufjf.br>
2022-07-03 00:55:30 -0300

    finish cripto game

---
+++ commons.hs
@@ -0,0 +1,79 @@
+--Nome: Fernanda Gonçalves da Silva
+--Matrícula: 201765102C
+
+module Commons where
+import System.Random
+
+-- generate a list of int
+generateSequence :: Int -> IO ([Int])
+generateSequence 0 = return []
+generateSequence n = do
+  r  <- randomRIO (1::Int, 9) 
+  rs <- generateSequence (n-1)
+  return (r:rs) 
+
+
+-- verify if two lists are equals
+verify :: [Int] -> [Int] -> Bool
+verify [] [] = True
+verify [] _ = False
+verify _ [] = False
+verify (x1:xs1) (x2:xs2) | (x1 == x2) = verify xs1 xs2
+                         | otherwise = False
+
+
+-- Converts an user entry to an integer list
+convert :: Read a => String -> [a]
+convert = map read . words
+
+
+-- Verify the count of completed positions between two lists
+completedPositions :: [Int] -> [Int] -> Int
+completedPositions [] [] = 0 
+completedPositions (x:xs) (y:ys)
+                | (x == y) = (completedPositions xs ys)+1
+                | otherwise = (completedPositions xs ys)
+
+
+-- Removes all completed positions and return a list with the others elements
+removeCompletedPositions :: [Int] -> [Int] -> [Int]
+removeCompletedPositions [] [] = [] 
+removeCompletedPositions (x:xs) (y:ys) =
+        if (x == y) 
+        then do
+                removeCompletedPositions xs ys
+         else do
+                [x]++(removeCompletedPositions xs ys)
+                
+
+-- Removes a partial element from the list
+removePartialElement :: Int -> [Int] -> [Int]
+removePartialElement element [] = [] 
+removePartialElement element (x:xs) =
+                if (x == element) 
+                then do
+                    removePartialElement element xs
+                else do
+                    [x]++(removePartialElement element xs)
+
+
+-- Verify if an element exist in the list
+verifyPartialPosition :: Int -> [Int] -> Bool
+verifyPartialPosition _ [] = False 
+verifyPartialPosition element (x:xs)
+                | (x == element) = True
+                | otherwise = (verifyPartialPosition element xs)
+
+
+-- Count the number of partial elements in the list
+partialCount :: [Int] -> [Int] -> Int
+partialCount [] _ = 0 
+partialCount (x:xs) sequence =
+    do
+        let existPartialPosition = verifyPartialPosition x sequence 
+        if (existPartialPosition == True)
+        then do
+            let newSequence = removePartialElement x sequence
+            (partialCount xs newSequence)+1
+        else do 
+            partialCount xs sequence
--- main.hs
+++ main.hs
@@ -1,14 +1,34 @@
+--Nome: Fernanda Gonçalves da Silva
+--Matrícula: 201765102C
 
--- Add random function where is number 2 to generate sequence
-main = do 
-            verifyPositions 2
+import Commons
 
-verifyPositions sequenceRandom = do 
-                                      putStr "What's the sequence? " 
-                                      sequenceUser <- getLine
-                                      if(read sequenceUser) == sequenceRandom
-                                          then do 
-                                                putStrLn "You WIN!"
-                                          else do
-                                               putStrLn "Not this time :("
-                                               verifyPositions sequenceRandom
+main = do sequenceRandom <- generateSequence 4
+          startGame sequenceRandom 0
+
+startGame sequenceRandom count = 
+         do putStr "What's the sequence? " 
+            userEntry <- getLine
+            let sequenceUserEntry = convert userEntry
+            let completedCount = completedPositions sequenceUserEntry sequenceRandom
+            let newSequenceRandom = removeCompletedPositions sequenceUserEntry sequenceRandom
+            let newSequenceUser = removeCompletedPositions sequenceRandom sequenceUserEntry
+            let partialPositions = partialCount newSequenceUser newSequenceRandom
+            let totalPositions = 4
+            let rounds = count + 1
+            print sequenceRandom
+            if completedCount == totalPositions
+            then do 
+                  putStr "Completed: "
+                  print completedCount
+                  putStr "Partial: "
+                  print partialPositions
+                  putStr "Congratulations! You won after "
+                  putStr (show rounds)
+                  putStrLn " rounds."
+            else do
+                  putStr "Completed: "
+                  print completedCount
+                  putStr "Partial: "
+                  print partialPositions
+                  startGame sequenceRandom rounds
